cb4f87d0f8f74cab07698aa5a3dd8e3ab9bd2b21
AbdulRehman7590 <theleostar3@gmail.com>
2024-02-25 13:10:44 +0500

    Connecting database with winforms

---
+++ Lab1/task1/task1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("Insert into Student values (@ID, @Name,@Department)", con);
+            cmd.Parameters.AddWithValue("@ID", textBox1.Text);
+            cmd.Parameters.AddWithValue("@Name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@Department", textBox3.Text);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Successfully saved");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("Select * from Student", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+    }
+}
